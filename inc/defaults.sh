48c45ceb12cf03fb4e21a9cc7f4a516c2f92a936
Yura <virt780@gmail.com>
2022-01-14 17:04:44 +0200

    fix

--- inc/defaults.sh
+++ inc/defaults.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Hardcode set gtk-theme and icon-theme
+# Default gtk-theme
+GTK_THEME_NAME="FlatColor"
+# Default icon-theme with colors modified variant
+GTK_ICON_THEME_NAME="Tela"
+# Default steam theme name
+STEAM_THEME_NAME="Adaptive"
+
 GTK_FONT_NAME="Noto Sans 11"
 GTK_TOOLBAR_STYLE="GTK_TOOLBAR_ICONS"
 GTK_TOOLBAR_ICON_SIZE="GTK_ICON_SIZE_SMALL_TOOLBAR"
@@ -14,3 +22,4 @@ COLOR[url_color]=#93B9D9
 COLOR[highlight]=#E45959
 
 COLOR[cursor]=#FFA500
+
