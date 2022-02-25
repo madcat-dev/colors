3dd8a84827468dcfebff87728d33b93679c519d9
Yura <virt780@gmail.com>
2022-02-25 22:35:26 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env bash
 
-APP_NAME='colors'
-
 LC_ALL=C
+
+APP_NAME="colors"
+
+SHARE="$HOME/.local/share"
+BIN="$HOME/.local/bin"
+
+
+while [ -n "$1" ]; do
+    case "${1}" in
+    --app)
+        APP_NAME="${2}"
+        ;;
+
+    --prefix)
+        SHARE="${2}/share"
+        BIN="${2}/bin"
+        ;;
+
+    --path)
+        SHARE="${2}/share"
+        BIN="${2}"
+        ;;
+    esac
+    shift
+done
+
+echo "app:   $APP_NAME"
+echo "bin:   $BIN/$APP_NAME"
+echo "share: $SHARE/$APP_NAME"
