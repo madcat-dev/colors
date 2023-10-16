a89d5163df5d4fd97e5879444ee63708586ec997
Yura <virt780@gmail.com>
2023-10-16 06:07:27 +0300

    fix

--- templates/gtk-theme/FlatColor/install.sh
+++ templates/gtk-theme/FlatColor/install.sh
@@ -13,7 +13,7 @@ debug "Install gtk theme '$THEME'"
 
 mkdir -p "$THEME_DIR"        2>/dev/null
 
-#rm   -rf "$THEME_DIR/$THEME" 2>/dev/null
+rm   -rf "$THEME_DIR/$THEME" 2>/dev/null
 
 cp -xarf "$MODULE/$THEME" "$THEME_DIR/" || return 1
 
@@ -25,3 +25,7 @@ apply "$THEME_DIR/$THEME/gtk-3.0/gtk.css.base" \
 
 apply "$THEME_DIR/$THEME/gtk-3.20/gtk.css.base" \
     "$THEME_DIR/$THEME/gtk-3.20/gtk.css" || return 1
+
+if [ ! -e "$THEME_DIR/XTheme" ]; then
+    ln -s "$THEME_DIR/$THEME" "$THEME_DIR/XTheme" || return 1
+fi
